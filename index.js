edd55e9bcca38aec9863a9d1f9b9e3e016c119c1
bhavin_patel <bhavin.patel@ua.edu>
2016-09-28 09:34:05 -0500

    rc-433

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //https://www.npmjs.com/package/rpi-433
 var rpi433    = require('rpi-433');
 // initialize rpi-433 module
-var rfSniffer = rpi433.sniffer(config.platforms.rpi['sniff-pin'], config.platforms.rpi['debounce-delay']); //Snif on PIN 2 with a 500ms debounce delay
+var rfSniffer = rpi433.sniffer(2, 500); //Snif on PIN 2 with a 500ms debounce delay
 var rfSend    = rpi433.sendCode;
 
 rfSniffer.on('codes', function(data){

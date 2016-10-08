16e3fa54e2ef7365d8ee0127995ff3fd575fa3f9
Bhavin Patel <bpatel@caps.ua.edu>
2016-10-07 23:06:01 -0500

    adding rcswitch

--- rcswitch.js
+++ rcswitch.js
@@ -7,18 +7,5 @@ var pin = 27;
 var rcswitch = require('rcswitch'); // Might throw an error if wiring pi init failed, or exit process if no root (must work on that)
 
 rcswitch.enableTransmit(0); // Use data Pin 0
-rcswitch.switchOn("10110", 1); // Switch on the first unit of 10110 (code 1x23x) group
-rcswitch.switchOff("11000", 2); // Switch off the second unit of 11000 (code 12xxx) group
-
-
-if(rcswitch.enableReceive(pin)) {
-  setInterval(function() {
-      if (rcswitch.available()) {
-          console.log(rcswitch.getReceivedValue());
-          rcswitch.resetAvailable();
-      }
-  }, 1000);
-}
-
+rcswitch.send(4199875);
 console.log("rcswitch running...");
-
